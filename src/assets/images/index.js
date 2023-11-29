941c530a775ed0b2685698e86b449613d1f500dd
Ali Naqi Hassan <ali.naqi@mmpakistan.com>
2023-11-29 18:12:41 +0500

    images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -8,22 +8,22 @@ import saleImgTwo from "./sale/saleImgTwo.webp";
 import saleImgThree from "./sale/saleImgThree.webp";
 // ============== Products Start here ====================
 // New Arrivals
-import newArrOne from "./products/newArrival/newArrOne.webp";
-import newArrTwo from "./products/newArrival/newArrTwo.webp";
-import newArrThree from "./products/newArrival/newArrThree.webp";
-import newArrFour from "./products/newArrival/newArrFour.webp";
+import newArrOne from "./products/newArrival/newArrOne.jpg";
+import newArrTwo from "./products/newArrival/newArrTwo.jpg";
+import newArrThree from "./products/newArrival/newArrThree.jpg";
+import newArrFour from "./products/newArrival/newArrFour.jpg";
 
 // Best Sellers
-import bestSellerOne from "./products/bestSeller/bestSellerOne.webp";
-import bestSellerTwo from "./products/bestSeller/bestSellerTwo.webp";
-import bestSellerThree from "./products/bestSeller/bestSellerThree.webp";
-import bestSellerFour from "./products/bestSeller/bestSellerFour.webp";
+import bestSellerOne from "./products/bestSeller/bestSellerOne.jpg";
+import bestSellerTwo from "./products/bestSeller/bestSellerTwo.jpg";
+import bestSellerThree from "./products/bestSeller/bestSellerThree.jpg";
+import bestSellerFour from "./products/bestSeller/bestSellerFour.jpg";
 
 // Special Offers
 import spfOne from "./products/specialOffer/spfOne.jpg";
-import spfTwo from "./products/specialOffer/spfTwo.webp";
-import spfThree from "./products/specialOffer/spfThree.webp";
-import spfFour from "./products/specialOffer/spfFour.webp";
+import spfTwo from "./products/specialOffer/spfTwo.jpg";
+import spfThree from "./products/specialOffer/spsThree.jpg";
+import spfFour from "./products/specialOffer/spfFour.jpg";
 
 // Year Product
 import productOfTheYear from "./products/productOfTheYear.webp";
